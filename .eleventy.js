a9e6d0a7a8a178c1f14278ca63daa4ba02333a27
Kiri Vadivelu <kiri.vadivelu@gmail.com>
2022-12-23 16:58:34 -0500

    multiple updates

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/img");
 
-  eleventyConfig.addPassthroughCopy("./src/404.md");
-
   eleventyConfig.addPassthroughCopy("./favicon.ico");
   eleventyConfig.addPassthroughCopy("./site.webmanifest");
   eleventyConfig.addPassthroughCopy("./favicon-16x16.png");
   eleventyConfig.addPassthroughCopy("./favicon-32x32.png");
   eleventyConfig.addPassthroughCopy("./apple-touch-icon.png");
-  eleventyConfig.addPassthroughCopy("./android-chrome-192x192.png");
-  eleventyConfig.addPassthroughCopy("./android-chrome-512x512.png");
   return {
     dir: {
       input: "src",
